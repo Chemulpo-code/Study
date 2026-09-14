6af0b64a55f771f4085b45cd9d7590dff2d0efb9
Chemulpo-code <perserilich@gmail.com>
2026-09-14 22:39:23 +1000

    test: remove swipe handler lint warning

--- src/components/SwipeBackHandler.test.jsx
+++ src/components/SwipeBackHandler.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, act } from '@testing-library/react';
+import { render, act } from '@testing-library/react';
 import { describe, it, expect, vi } from 'vitest';
 import SwipeBackHandler from './SwipeBackHandler';
 
